3884bf1ec6c8de53d742ec26dfd1fb294a594c8e
JustinFreitas <justin.s.freitas@me.com>
2022-10-07 17:32:04 -0600

    updates

--- scripts/ration-expiration-date.js
+++ scripts/ration-expiration-date.js
@@ -5,7 +5,7 @@ Hooks.on('createItem', (document) => {
             || document.name === 'Rations, iron')) {
         const expirationDateValue = document.name.endsWith('standard') ? 6.048e+8 * 2 : 6.048e+8 * 4;
         const expirationDate = new Date(Date.now() + expirationDateValue);
-        document.name = `${document.name} (${expirationDate.toLocaleDateString()})`;
-        document._source.name = document.name; // Required to maintain name when new items are dropped in.
+        const updatedName = `${document.name} (${expirationDate.toLocaleDateString()})`;
+        document.update({ name: updatedName });
     }
 });
