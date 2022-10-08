e61abb9ac287338faa011e88336c61e87c9c1833
JustinFreitas <justin.s.freitas@me.com>
2022-10-07 19:12:29 -0600

    use preCreateItem instead of createItem

--- scripts/ration-expiration-date.js
+++ scripts/ration-expiration-date.js
@@ -1,11 +1,10 @@
-Hooks.on('createItem', (document) => {
-    if (document
-        && document.type === 'item'
-        && (document.name === 'Rations, standard'
-            || document.name === 'Rations, iron')) {
+Hooks.on('preCreateItem', (document) => {
+    if (document?.type === 'item'
+        && (document?.name === 'Rations, standard'
+            || document?.name === 'Rations, iron')) {
         const expirationDateValue = document.name.endsWith('standard') ? 6.048e+8 * 2 : 6.048e+8 * 4;
         const expirationDate = new Date(Date.now() + expirationDateValue);
         const updatedName = `${document.name} (${expirationDate.toLocaleDateString()})`;
-        document.update({ name: updatedName });
+        document.data.update({ name: updatedName });
     }
 });
