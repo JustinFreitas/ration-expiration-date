1bb8a43ed72fd524be7cdbcb2be864d4bf776e4e
JustinFreitas <justin.s.freitas@me.com>
2022-10-07 19:19:47 -0600

    constant for magic number

--- scripts/ration-expiration-date.js
+++ scripts/ration-expiration-date.js
@@ -2,7 +2,8 @@ Hooks.on('preCreateItem', (document) => {
     if (document?.type === 'item'
         && (document?.name === 'Rations, standard'
             || document?.name === 'Rations, iron')) {
-        const expirationDateValue = document.name.endsWith('standard') ? 6.048e+8 * 2 : 6.048e+8 * 4;
+        const week = 6.048e+8;
+        const expirationDateValue = document.name.endsWith('standard') ? week * 2 : week * 4;
         const expirationDate = new Date(Date.now() + expirationDateValue);
         const updatedName = `${document.name} (${expirationDate.toLocaleDateString()})`;
         document.data.update({ name: updatedName });
