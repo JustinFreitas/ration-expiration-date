05f02a2553b3bf086c2a5884804bb9e257777ddb
JustinFreitas <justin.s.freitas@me.com>
2022-10-09 11:28:20 -0600

    system relationship and config settings

--- scripts/ration-expiration-date.js
+++ scripts/ration-expiration-date.js
@@ -1,11 +1,73 @@
+Hooks.on('init', function() {
+    const moduleName = 'ration-expiration-date';
+    const world = 'world';
+
+    game.settings.register(moduleName, 'ironRationsItemName', {
+        name: 'Iron Rations Item Name',
+        hint: 'The item name for Iron Rations that will be used to match on inventory addition for expiration date stamping.',
+        scope: world,     // "world" = sync to db, "client" = local storage
+        config: true,       // false if you dont want it to show in module config
+        type: String,       // Number, Boolean, String, Object
+        default: 'Rations, iron',
+        onChange: value => { // value is the new value of the setting
+          console.log(value);
+        },
+    });
+
+    game.settings.register(moduleName, 'ironRationsExpireDays', {
+        name: 'Iron Rations Expire Days',
+        hint: 'The number of days until iron rations expiration.',
+        scope: world,
+        config: true,
+        type: Number,
+        default: 28,
+        onChange: value => {
+          console.log(value);
+        },
+    });
+
+    game.settings.register(moduleName, 'standardRationsItemName', {
+        name: 'Standard Rations Item Name',
+        hint: 'The item name for Standard Rations that will be used to match on inventory addition for expiration date stamping.',
+        scope: world,
+        config: true,
+        type: String,
+        default: 'Rations, standard',
+        onChange: value => {
+          console.log(value);
+        },
+    });
+      
+    game.settings.register(moduleName, 'standardRationsExpireDays', {
+        name: 'Standard Rations Expire Days',
+        hint: 'The number of days until standard rations expiration.',
+        scope: world,
+        config: true,
+        type: Number,
+        default: 14,
+        onChange: value => {
+          console.log(value);
+        },
+    });
+});
+  
 Hooks.on('preCreateItem', (document) => {
-    if (document?.type === 'item'
-        && (document?.name === 'Rations, standard'
-            || document?.name === 'Rations, iron')) {
-        const week = 6.048e+8;
-        const expirationDateValue = document.name.endsWith('standard') ? week * 2 : week * 4;
-        const expirationDate = new Date(Date.now() + expirationDateValue);
-        const updatedName = `${document.name} (${expirationDate.toLocaleDateString()})`;
-        document.data.update({ name: updatedName });
+    if (document?.type === 'item') {
+        const moduleName = 'ration-expiration-date';
+        const ironRationsItemName = game.settings.get(moduleName, 'ironRationsItemName');
+        const standardRationsItemName = game.settings.get(moduleName, 'standardRationsItemName');
+
+        if (document.name === ironRationsItemName
+            || document.name === standardRationsItemName) {
+            const day = 8.64e+7;
+            const ironRationsExpireDays = game.settings.get(moduleName, 'ironRationsExpireDays');
+            const standardRationsExpireDays = game.settings.get(moduleName, 'standardRationsExpireDays');
+            const expirationDateValue = document.name === standardRationsItemName
+                                            ? day * standardRationsExpireDays
+                                            : day * ironRationsExpireDays;
+            const expirationDate = new Date(Date.now() + expirationDateValue);
+            const updatedName = `${document.name} (${expirationDate.toLocaleDateString()})`;
+            document.data.update({ name: updatedName });
+        }
     }
 });
