eaf809a44afb4e8feca560015c654f6db7fa411d
JustinFreitas <justin.s.freitas@me.com>
2022-11-13 16:47:07 -0700

    scripts for building release zip

---
+++ build/build-ration-expiration-date-zip.bat
@@ -0,0 +1,8 @@
+:: Assumes running from ration-expiration-date\build
+mkdir out\ration-expiration-date\scripts
+copy ..\module.json out\ration-expiration-date\
+copy ..\scripts\ration-expiration-date.js out\ration-expiration-date\scripts\
+cd out
+CALL ..\zip ration-expiration-date
+rmdir /S /Q ration-expiration-date\
+cd ..
