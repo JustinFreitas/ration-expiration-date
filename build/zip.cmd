2f5e12d4d2637359df6d7fad8508bcea3953b2d7
JustinFreitas <justin.s.freitas@me.com>
2022-11-13 16:55:02 -0700

    zip script fix for zipping folder vs contents

--- build/zip.cmd
+++ build/zip.cmd
@@ -2,7 +2,7 @@
 if "%1"=="" goto end
 
 setlocal
-set TEMPDIR=%TEMP%\ZIP
+set TEMPDIR=%TEMP%\%1
 set FILETOZIP=%1
 set OUTPUTZIP=%2.zip
 if "%2"=="" set OUTPUTZIP=%1.zip
@@ -16,7 +16,7 @@ echo Set objZipFile = fso.CreateTextFile(ZipFile, True) >> _zipIt.vbs
 echo objZipFile.Write "PK" ^& Chr(5) ^& Chr(6) ^& String(18, vbNullChar) >> _zipIt.vbs
 echo objZipFile.Close >> _zipIt.vbs
 echo Set objShell = WScript.CreateObject("Shell.Application") >> _zipIt.vbs
-echo Set source = objShell.NameSpace(InputFolder).Items >> _zipIt.vbs
+echo Set source = objShell.NameSpace(InputFolder) >> _zipIt.vbs
 echo Set objZip = objShell.NameSpace(fso.GetAbsolutePathName(ZipFile)) >> _zipIt.vbs
 echo if not (objZip is nothing) then  >> _zipIt.vbs
 echo    objZip.CopyHere(source) >> _zipIt.vbs
