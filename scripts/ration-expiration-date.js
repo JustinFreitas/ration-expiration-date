877159952524bdb626d7d535abbb24a94281aff1
JustinFreitas <justin.s.freitas@me.com>
2022-11-24 10:43:16 -0700

    Use constants for redundant strings and cleanup.

--- scripts/ration-expiration-date.js
+++ scripts/ration-expiration-date.js
@@ -1,56 +1,64 @@
+const MODULE_NAME = 'ration-expiration-date';
+const IRON_RATIONS_ITEM_NAME = 'ironRationsItemName';
+const IRON_RATIONS_ITEM_NAME_DEFAULT = 'Rations, iron';
+const IRON_RATIONS_EXPIRE_DAYS = 'ironRationsExpireDays';
+const IRON_RATIONS_EXPIRE_DAYS_DEFAULT = 56;
+const STANDARD_RATIONS_ITEM_NAME = 'standardRationsItemName';
+const STANDARD_RATIONS_ITEM_NAME_DEFAULT = 'Rations, standard';
+const STANDARD_RATIONS_EXPIRE_DAYS = 'standardRationsExpireDays';
+const STANDARD_RATIONS_EXPIRE_DAYS_DEFAULT = 14;
+
 Hooks.on('init', function() {
-    const moduleName = 'ration-expiration-date';
     const world = 'world';
 
-    game.settings.register(moduleName, 'ironRationsItemName', {
+    game.settings.register(MODULE_NAME, IRON_RATIONS_ITEM_NAME, {
         name: 'Iron Rations Item Name',
         hint: 'The item name for Iron Rations that will be used to match on inventory addition for expiration date stamping.',
-        scope: world,     // "world" = sync to db, "client" = local storage
-        config: true,       // false if you dont want it to show in module config
-        type: String,       // Number, Boolean, String, Object
-        default: 'Rations, iron',
+        scope: world,
+        config: true,
+        type: String,
+        default: IRON_RATIONS_ITEM_NAME_DEFAULT,
     });
 
-    game.settings.register(moduleName, 'ironRationsExpireDays', {
+    game.settings.register(MODULE_NAME, IRON_RATIONS_EXPIRE_DAYS, {
         name: 'Iron Rations Expire Days',
         hint: 'The number of days until iron rations expiration.',
         scope: world,
         config: true,
         type: Number,
-        default: 56,
+        default: IRON_RATIONS_EXPIRE_DAYS_DEFAULT,
     });
 
-    game.settings.register(moduleName, 'standardRationsItemName', {
+    game.settings.register(MODULE_NAME, STANDARD_RATIONS_ITEM_NAME, {
         name: 'Standard Rations Item Name',
         hint: 'The item name for Standard Rations that will be used to match on inventory addition for expiration date stamping.',
         scope: world,
         config: true,
         type: String,
-        default: 'Rations, standard',
+        default: STANDARD_RATIONS_ITEM_NAME_DEFAULT,
     });
       
-    game.settings.register(moduleName, 'standardRationsExpireDays', {
+    game.settings.register(MODULE_NAME, STANDARD_RATIONS_EXPIRE_DAYS, {
         name: 'Standard Rations Expire Days',
         hint: 'The number of days until standard rations expiration.',
         scope: world,
         config: true,
         type: Number,
-        default: 14,
+        default: STANDARD_RATIONS_EXPIRE_DAYS_DEFAULT,
     });
 });
   
 Hooks.on('preCreateItem', (document) => {
-    const moduleName = 'ration-expiration-date';
-    const ironRationsItemName = game.settings.get(moduleName, 'ironRationsItemName') || 'Rations, iron';
-    const standardRationsItemName = game.settings.get(moduleName, 'standardRationsItemName') || 'Rations, standard';
+    const ironRationsItemName = game.settings.get(MODULE_NAME, IRON_RATIONS_ITEM_NAME) || IRON_RATIONS_ITEM_NAME_DEFAULT;
+    const standardRationsItemName = game.settings.get(MODULE_NAME, STANDARD_RATIONS_ITEM_NAME) || STANDARD_RATIONS_ITEM_NAME_DEFAULT;
 
     if (document?.ownership
         && Object.keys(document.ownership).length > 1
         && (document?.name === ironRationsItemName
             || document?.name === standardRationsItemName)) {
         const day = 8.64e+7;
-        const ironRationsExpireDays = game.settings.get(moduleName, 'ironRationsExpireDays') || 56;
-        const standardRationsExpireDays = game.settings.get(moduleName, 'standardRationsExpireDays') || 14;
+        const ironRationsExpireDays = game.settings.get(MODULE_NAME, IRON_RATIONS_EXPIRE_DAYS) || IRON_RATIONS_EXPIRE_DAYS_DEFAULT;
+        const standardRationsExpireDays = game.settings.get(MODULE_NAME, STANDARD_RATIONS_EXPIRE_DAYS) || STANDARD_RATIONS_EXPIRE_DAYS_DEFAULT;
         const expirationDateValue = document.name === standardRationsItemName
                                         ? day * standardRationsExpireDays
                                         : day * ironRationsExpireDays;
